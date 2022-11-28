0e61fd6fb264445ac6ae7807beb9ce947758d3b9
Junghwan Han <therealpizzaroot@gmail.com>
2022-11-28 19:25:34 +0900

    [Bronze IV] Title: A Simple Problem., Time: 40 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/15372. A Simple Problem．/A Simple Problem．.cc
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+	ios::sync_with_stdio(0); cin.tie(0);
+	int t; cin >> t;
+	while (t--) {
+		long long  n; cin >> n;
+		cout << n * n << "\n";
+	}
+	return 0;
+}
