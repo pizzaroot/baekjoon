68e4b9376f6ecc8b52ddf73895383af9e56c0e9e
Junghwan Han <therealpizzaroot@gmail.com>
2022-11-29 08:41:42 +0900

    [Bronze IV] Title: Another Eruption, Time: 0 ms, Memory: 2032 KB -BaekjoonHub

---
+++ 백준/Bronze/21335. Another Eruption/Another Eruption.cc
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+#define PI 3.14159265358979
+using namespace std;
+int main() {
+	ios::sync_with_stdio(0); cin.tie(0);
+	long long a; cin >> a;
+	cout << setprecision(9) << 2 * sqrt(a * PI) << "\n";
+	return 0;
+}
