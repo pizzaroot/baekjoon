3f3e572c0a81022ae04832ef388437f0bf5716ef
Junghwan Han <therealpizzaroot@gmail.com>
2022-11-28 19:23:12 +0900

    [Bronze IV] Title: Communication Channels, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/11121. Communication Channels/Communication Channels.cc
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+	ios::sync_with_stdio(0); cin.tie(0);
+	int t; cin >> t;
+	while (t--) {
+		string a, b; cin >> a >> b;
+		if (a == b) cout << "OK\n";
+		else cout << "ERROR\n";
+	}
+	return 0;
+}
