cca40be77335f5621d68ea8170cccac36469eed9
Junghwan Han <therealpizzaroot@gmail.com>
2022-11-28 19:23:37 +0900

    [Bronze IV] Title: Schronisko, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/8760. Schronisko/Schronisko.cc
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+	ios::sync_with_stdio(0); cin.tie(0);
+	int z; cin >> z;
+	while (z--) {
+		int w, k; cin >> w >> k;
+		cout << w * k / 2 << "\n";
+	}
+	return 0;
+}
