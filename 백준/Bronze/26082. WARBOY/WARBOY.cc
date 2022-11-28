17dd9207375098235b4e3cba2a9579c2afad9875
Junghwan Han <therealpizzaroot@gmail.com>
2022-11-28 19:25:46 +0900

    [Bronze V] Title: WARBOY, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/26082. WARBOY/WARBOY.cc
@@ -0,0 +1,8 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+	ios::sync_with_stdio(0); cin.tie(0);
+	int a, b, c; cin >> a >> b >> c;
+	cout << 3 * c * b / a << "\n";
+	return 0;
+}
