20ac2c4e0ab512d41d031af0d0556b8434ffe976
Junghwan Han <therealpizzaroot@gmail.com>
2022-12-12 22:31:56 +0900

    [level 3] Title: 야근 지수, Time: 10.90 ms, Memory: 3.91 MB -BaekjoonHub

--- 프로그래머스/lv3/12927. 야근 지수/야근 지수.cpp
+++ 프로그래머스/lv3/12927. 야근 지수/야근 지수.cpp
@@ -3,7 +3,7 @@ using namespace std;
 long long solution(int n, vector<int> works) {
     long long answer = 0;
     int limit = max(0LL, accumulate(works.begin(), works.end(), 0LL) - n) / works.size();
-    int repeat = n / works.size() + 1, cur = *max_element(works.begin(), works.end());
+    int cur = *max_element(works.begin(), works.end());
     while (cur >= limit) {
         for (int i = 0; i < works.size() && n; i++) {
             if (works[i] > cur) n--, works[i]--;
