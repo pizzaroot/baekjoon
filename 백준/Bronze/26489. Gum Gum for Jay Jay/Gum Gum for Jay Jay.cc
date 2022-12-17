6be5e99707a39d953eac1e7cefb16007af39f920
Junghwan Han <therealpizzaroot@gmail.com>
2022-12-18 04:26:02 +0900

    [Bronze V] Title: Gum Gum for Jay Jay, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/26489. Gum Gum for Jay Jay/Gum Gum for Jay Jay.cc
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    ios::sync_with_stdio(0); cin.tie(0);
+    int ans = 0; string s;
+    while (getline(cin, s)) ans++;
+    cout << ans << "\n";
+    return 0;
+}
